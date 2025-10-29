efb8f0d7ead57606f1e4fa392ec33a647c786fc1
ArmanArdavani <arman.ardavani@gmail.com>
2025-10-30 02:22:30 +0330

    Add files via upload

---
+++ 3.Nested loops.py
@@ -0,0 +1,13 @@
+#First Case 
+for x in range(6):
+    for y in range(3):
+        print(f"({x}, {y})")
+print("First case done")
+
+
+#Second Case 
+for x in range(1,7):
+    for y in range(1,4):
+        print(f"{x}, {y}")
+print("2nd case done")
+    
